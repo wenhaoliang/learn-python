c7a92ab9e40c836f5c38088e7c3bf0d84700ed96
wenhaoliang <641541452@qq.com>
2016-05-06 12:07:46 +0800

    del

--- Python练习册，每天一个小程序/0008/1_files/index.php
+++
@@ -1,1 +0,0 @@
-document.getElementById('login_status').innerHTML='<div class="login" style="padding-top:8px; text-align:right;"><a href="http://home.51cto.com/index?reback=http://developer.51cto.com/art/201003/190605.htm" target="_self">登录</a><a href="http://ucenter.51cto.com/2015/register.php?reback=http://developer.51cto.com/art/201003/190605.htm" target="_self">注册</a></div>';

e8c51ad0fc94d4ba6511d9141bad19f6ed28ff4d
wenhaoliang <641541452@qq.com>
2016-05-07 20:03:11 +0800

    second

--- django/practice/practice/urls.py
+++ django/practice/practice/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from learn import views as learn_views  # new
  
 urlpatterns = [
-    url(r'^$', learn_views.index),  # new
+    url(r'^$',learn_views.index),  # new
+	#url(r'^$', learn_views.index),
     url(r'^admin/', admin.site.urls),
 ]

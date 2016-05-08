e8b08326b891c8ea8579dd8be06a0a70266ae9e8
wenhaoliang <641541452@qq.com>
2016-05-08 13:03:42 +0800

    ninth

--- django/zqxt_views/learn/views.py
+++ django/zqxt_views/learn/views.py
@@ -26,3 +26,7 @@ def home(request):
 def home(request):
 	info_dict = {'site': u"大懒虫", 'content':u"奇妙的世界"}
 	return render(request, "home.html", {'info_dict': info_dict})
+
+def home(request):
+	List = map(str, range(1000)) 
+	return render(request, 'home.html', {'List': List})

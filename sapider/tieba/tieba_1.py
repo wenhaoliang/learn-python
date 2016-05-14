0d66a2c5cdec8a1ddb8cf1cd899d8c38bb1fe115
wenhaoliang <641541452@qq.com>
2016-05-14 22:35:29 +0800

    Spider

---
+++ sapider/tieba/tieba_1.py
@@ -0,0 +1,80 @@
+# -*- coding:utf-8 -*-
+import urllib
+import urllib2
+import re
+
+#百度贴吧类
+class BDTB:
+
+    def __init__(self, baseUrl, seeLZ ):
+        self.baseUrl = baseUrl
+        self.seeLZ = "?see_Lz=" + str(seeLZ)
+
+    #传入页码，获取该页帖子代码
+    def getPage(self, pageNum):
+        try:
+            url = self.baseUrl + self.seeLZ + '&pn=' + str(pageNum)
+            request = urllib2.Request(url)
+            response = urllib2.urlopen(request)
+            print response.read()
+            return response
+        except urllib2.URLError, e:
+            if hasattr(e, "reason"):
+                print u"链接失败", e.reason
+                return None
+
+    def getTitle(self):
+        page = self.getPage(1)
+        pattern = re.compile('<h3 class="core_title_txt.*?>(.*?)</h3>',re.S)
+        result = re.search(pattern, page)
+        if result:
+            print result.group(1)
+            return result.group(1).strip()
+        else:
+            return None
+
+    def getPageNum(self):
+        page = self.getPage(1)
+        pattern = re.compile('<li class="l_reply_num.*?</span>.*?<span.*?>(.*?)</span>', re.S)
+        result = re.search(pattern, page)
+        if result:
+            print result.group(1)
+            return result.group(1).strip()
+        else:
+            return None
+
+    def  getContent(self, page):
+        pattern = re.compile('<div id="post_content_.*?>(.*?)</div>', re.S)
+        items = re.findall(pattern, page)
+        for item in items:
+            print item
+
+class Tool:
+    #去除img标签,7位长空格
+    removeImg = re.compile('<img.*?>| {7}|')
+    #删除超链接标签
+    removeAddr = re.compile('<a.*?>|</a>')
+    #把换行的标签换为\n
+    replaceLine = re.compile('<tr>|<div>|</div>|</p>')
+    #将表格制表<td>替换为\t
+    replaceTD= re.compile('<td>')
+    #把段落开头换为\n加空两格
+    replacePara = re.compile('<p.*?>')
+    #将换行符或双换行符替换为\n
+    replaceBR = re.compile('<br><br>|<br>')
+    #将其余标签剔除
+    removeExtraTag = re.compile('<.*?>')
+    def replace(self,x):
+        x = re.sub(self.removeImg,"",x)
+        x = re.sub(self.removeAddr,"",x)
+        x = re.sub(self.replaceLine,"\n",x)
+        x = re.sub(self.replaceTD,"\t",x)
+        x = re.sub(self.replacePara,"\n    ",x)
+        x = re.sub(self.replaceBR,"\n",x)
+        x = re.sub(self.removeExtraTag,"",x)
+        #strip()将前后多余内容删除
+        return x.strip()
+
+baseURL = "http://tieba.baidu.com/p/3138733512"
+data = BDTB(baseURL,1)
+

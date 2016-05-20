a96f89bc80e3afb8160f92474a2a830ceefb3907
wenhaoliang <641541452@qq.com>
2016-05-20 22:44:39 +0800

    2016-05-20 22:34:55

--- Python实战：四周实现爬虫系统/week_2/2_1/2_1.py
+++ Python实战：四周实现爬虫系统/week_2/2_1/2_1.py
@@ -5,9 +5,8 @@ def get_info():
     #http://bj.xiaozhu.com/search-duanzufang-p1-0/
     base_url = 'http://bj.xiaozhu.com/search-duanzufang-p'
     for i in range(1,4):
-        full_url = base_url + str(i) + '-0'
+        full_url = base_url + str(i) + '-0/'
         wb_data = requests.get(full_url)
-        soup = BeautifulSoup(wb_data.text, 'lxml')
-        title = soup.select('result_title hiddenTxt')
-        print (title)
+        soup = BeautifulSoup(wb_data.text, 'html5lib')
+        title = soup.select('span.result_title')
 get_info()

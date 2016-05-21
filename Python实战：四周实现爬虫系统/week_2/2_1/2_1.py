fe9d99cb0e1c308d6a3bda863d5f65afc615b885
wenhaoliang <641541452@qq.com>
2016-05-21 10:31:22 +0800

    2016-05-21 10:31:20

--- Python实战：四周实现爬虫系统/week_2/2_1/2_1.py
+++ Python实战：四周实现爬虫系统/week_2/2_1/2_1.py
@@ -1,5 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+import pymongo
+
+client = pymongo.MongoClient("localhost", 27017)
+xiaozhu = client['xiaozhu']
+duanzu_tab = xiaozhu['duanzu_tab']
 
 def get_info():
     #http://bj.xiaozhu.com/search-duanzufang-p1-0/
@@ -7,6 +12,22 @@ def get_info():
     for i in range(1,4):
         full_url = base_url + str(i) + '-0/'
         wb_data = requests.get(full_url)
-        soup = BeautifulSoup(wb_data.text, 'html5lib')
-        title = soup.select('span.result_title')
-get_info()
+        soup = BeautifulSoup(wb_data.text, 'lxml')
+        titles = soup.select('span.result_title')
+        prices = soup.select('span.result_price > i ')
+        #print(titles,prices)
+        for title, price in zip(titles, prices):
+            data = {
+                '房屋名字': title.get_text(),
+                '价格'    : int(price.get_text())
+            }
+            duanzu_tab.insert_one(data)
+            #print(data)
+if __name__ == "__main__":
+    get_info()
+    # $lt/$lte/$gt/$gte/$ne，依次等价于</<=/>/>=/!=。（l表示less g表示greater e表示equal n表示not  ）
+    for item in duanzu_tab.find():
+        if item['价格'] >= 500:
+            print(item)
+
+

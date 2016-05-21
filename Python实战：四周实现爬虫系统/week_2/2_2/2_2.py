59a3bd2e0f829582964e99ea6669d5ef1056d646
wenhaoliang <641541452@qq.com>
2016-05-21 14:40:10 +0800

    2016-05-21 14:40:08

--- Python实战：四周实现爬虫系统/week_2/2_2/2_2.py
+++ Python实战：四周实现爬虫系统/week_2/2_2/2_2.py
@@ -1,28 +1,28 @@
-    import requests
-    from bs4 import BeautifulSoup
-    import pymongo
-    import time
-    client = pymongo.MongoClient("localhost", 27017)
-    tongcheng = client['tongcheng']
-    tel_num = tongcheng['tel_num']
+import requests
+from bs4 import BeautifulSoup
+import pymongo
+import time
+client = pymongo.MongoClient("localhost", 27017)
+tongcheng = client['tongcheng']
+tel_num = tongcheng['tel_num']
 
-    def get_tel_num():
-        base_url = 'http://zz.58.com/shoujihao/pn{}/'
-        for pages in range(1,100):
-            full_url = base_url.format(str(pages))
-            time.sleep(2)
-            wb_data = requests.get(full_url)
-            soup = BeautifulSoup(wb_data.text, 'lxml')
-            titles = soup.select('strong.number')
-            hrefs = soup.select('a.t')
-            for title,href in zip(titles, hrefs):
-                data = {
-                    '手机号': title.get_text(),
-                    '链接'  : href.get('href')
-                }
-                tel_num.insert_one(data)
-                print(data)
+def get_tel_num():
+    base_url = 'http://zz.58.com/shoujihao/pn{}/'
+    for pages in range(1,1000):
+        full_url = base_url.format(str(pages))
+        #time.sleep(2)
+        wb_data = requests.get(full_url)
+        soup = BeautifulSoup(wb_data.text, 'lxml')
+        titles = soup.select('strong.number')
+        hrefs = soup.select('a.t')
+        for title,href in zip(titles, hrefs):
+            data = {
+                '手机号': title.get_text(),
+                '链接'  : href.get('href')
+            }
+            tel_num.insert_one(data)
+            print(data)
 
 
 
-    get_tel_num()
+get_tel_num()

be916c41e97ee5523ea7c124b674fe70f689ac18
梁文豪 <641541452@qq.com>
2016-05-31 13:00:24 +0800

    2016-05-31 13:00:21

--- Python实战：四周实现爬虫系统/week_2/第二周实战项目/shujuceshi.py
+++ Python实战：四周实现爬虫系统/week_2/第二周实战项目/shujuceshi.py
@@ -4,9 +4,9 @@ import pymongo
 
 client = pymongo.MongoClient('localhost', 27017)
 ganji = client['ganji']
-ganji_info = ganji['ganji_data1']
+ganji_info = ganji['ganji_url']
 
-for i in ganji_info.find():
-    print(i)
-
+# for i in ganji_info.find():
+#     print(i)
+print(ganji_info.find().count())
 

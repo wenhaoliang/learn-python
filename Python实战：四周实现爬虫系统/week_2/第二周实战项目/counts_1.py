f260ed797301535d3dd18093fcd0f3a116e79ca8
梁文豪 <641541452@qq.com>
2016-06-04 09:39:52 +0800

    2016-06-04 09:39:50

---
+++ Python实战：四周实现爬虫系统/week_2/第二周实战项目/counts_1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+import pymongo
+import time
+client = pymongo.MongoClient('localhost', 27017)
+ganji = client['ganji']
+ganji_data1 = ganji['ganji_data1']
+ganji_url = ganji['ganji_url']
+
+
+while True:
+    print(ganji_data1.find().count())
+    print("-" * 100)
+    a = ganji_data1.find().count()
+    time.sleep(3)
+    b = ganji_data1.find().count()
+    print(b-a)
+    print("-" * 100)

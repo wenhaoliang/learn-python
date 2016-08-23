8c12523e48ed082cee0367c6fbea79059bf8e7b0
梁文豪 <641541452@qq.com>
2016-08-23 11:20:24 +0800

    2016-08-23 11:18:54

---
+++ Python实战：四周实现爬虫系统/week_4/4_2/4_2answer_of_homework/Django_pure/pure/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.core.paginator import Paginator
+from pure.models import ItemInfo
+
+# Create your views here.
+
+def pure_index(request):
+    return render(request,'pure_index.html')
+
+
+
+def home(request):
+    limit = 10
+    item_info = ItemInfo.objects
+    paginatior = Paginator(item_info,limit)
+    page = request.GET.get('page',1)
+    print("-" * 100)
+    print(request)
+    print(request.GET)
+    print("-"  * 100)
+    loaded = paginatior.page(page)
+
+    context = {
+        'ItemInfo':loaded,
+    }
+
+    return render(request,'pure_index_paginator.html',context)
